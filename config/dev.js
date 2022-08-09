66e6ca22dd6508f6988b7c7ac96d7c434c220c7d
madeleineenberg <madeleine_enberg@hotmail.com>
2022-08-09 18:12:38 +0200

    mongoDB setup and connection

---
+++ config/dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+  DB_URI:
+    'mongodb+srv://madeleineenberg:k59tQ8Bd4saImgNd@cluster0.c1iidau.mongodb.net/?retryWrites=true&w=majority',
+};
--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const server = express();
+const config = require('./config/dev');
 
-server.get('/test', (req, res) => {
-  return res.json({ message: 'test is working' });
-});
+const mongoose = require('mongoose');
+
+mongoose.connect(
+  config.DB_URI,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log('Connected to DB');
+    }
+  }
+);
+
+server.use('/api/v1/portfolios', require('./routes/portfolios.js'));
 
 const PORT = parseInt(process.env.PORT, 10) || 3001;
 server.listen(PORT, (err) => {
