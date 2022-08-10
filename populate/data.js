7c066092073042b00784964cf94f162130254c7d
madeleineenberg <madeleine_enberg@hotmail.com>
2022-08-10 21:19:45 +0200

    populate mongoDB with

---
+++ populate/data.js
@@ -0,0 +1,36 @@
+const data = {
+  portfolios: [
+    {
+      title: 'First job as a developer',
+      company: 'Osolo',
+      companyWebsite: 'www.osolo.se',
+      location: 'Sweden',
+      jobTitle: 'Frontend developer',
+      description: 'Doing something, programing....',
+      startDate: '01/01/2014',
+      endDate: '01/01/2016',
+    },
+    {
+      title: 'Frilance photographer',
+      company: 'Madeleine Enberg photograpgy',
+      companyWebsite: 'www.madeleineenberg.com',
+      location: 'Sweden',
+      jobTitle: 'Photographer',
+      description: 'artsy stuff and whatever',
+      startDate: '01/01/2011',
+      endDate: '01/01/2013',
+    },
+    {
+      title: 'Prego',
+      company: 'WhoKnows',
+      companyWebsite: 'www.google.com',
+      location: 'Sweden',
+      jobTitle: 'manager',
+      description: 'So much responsibility....Overloaaaaaad',
+      startDate: '01/01/2010',
+      endDate: '01/01/2011',
+    },
+  ],
+};
+
+module.exports = data;
